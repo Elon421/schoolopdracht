6385efc7db7922291e8af9b34c111f91032eb65d
Elon421 <132984242+Elon421@users.noreply.github.com>
2023-05-09 09:05:06 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,23 @@
+ <?php
+    //$cijfer1 = 12;
+   // $cijfer2 = 10;
+
+//$resultaat = $cijfer1 + $cijfer2;
+
+//echo "12 + 10 = $resultaat";
+
+ //$cijfer1 = 12;
+   // $cijfer2 = 10;
+
+//$resultaat = $cijfer1 * $cijfer2;
+
+//echo "12 * 10 = $resultaat";
+
+//$naam = "Lakshya";
+
+//echo "Welkom " . $naam;
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
+?> 
