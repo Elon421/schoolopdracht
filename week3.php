2fd0833756174b1bb8446941af6f8444bce91cd0
Elon421 <132984242+Elon421@users.noreply.github.com>
2023-05-26 14:00:21 +0200

    Add files via upload

---
+++ week3.php
@@ -0,0 +1,30 @@
+<?php
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+//$Myarray = array('auto', 'fiets', 'brommer', 'bus', 'vliegtuig', 'trein', 'scooter');
+//echo $myArray[0];
+//echo $myArray[1];
+//echo $myArray[2];
+//echo $myArray[3];
+//echo $myArray[4];
+
+// echo $myArray[5];
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein', 'scooter'];
+//echo count($myArray);
+
+// op regel 2 word er bar uitgeprint
+// op regel 5 word er 4 uitgeprint
+// op regel 8 word er toyota uitgeprint
+// op regel 9 word het aantal autos opgeteld, dus 3 dingen worden opgeteld
+// op regel 12 word er 5 uitgeprint
+// op regel 19 worden alle autos opgeteld, behalve 1
+
+// $cijfersPHP = [4.4 , 4.6 , 5.6 , 6.1 , 7.6 , 7.2];
+// $aantalcijfers = count($cijfersPHP);
+// $gemiddelde = array_sum($cijfersPHP) / $aantalcijfers;
+// $gemiddelde = array_sum($cijfersPHP) / count($cijfersPHP);
+// echo "gemiddelde is:". $gemiddelde;
+// echo(round($gemiddelde , 2) . "<br>");
+
+//ik raad het aan om het in regel te doen, omdat het dan netjes lijkt en op 1 regel is
+?>
