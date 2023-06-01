bf06788548b6b4f781800dd9421eee5a062367c5
Elon421 <132984242+Elon421@users.noreply.github.com>
2023-06-01 22:14:33 +0200

    Add files via upload

---
+++ week4.php
@@ -0,0 +1,17 @@
+<?php
+// for ($i = 1; $i <= 50; $i++) {
+    // echo $i . " ";
+    // }
+
+    //$klasgenoten = array('Jan', 'Adriaan', 'Jaap', 'Henk', 'Mark', 'Jelle', 'Bas' , 'Joost' , 'Thomas' , 'Ronald');
+    //foreach ($klasgenoten as $naam) {
+     //   echo $naam . "<br>";
+    //  }
+
+//$maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];  
+
+//foreach ($maanden as $maand) {
+  //  echo 'Maand is '.$maand.'.<br />';
+//}
+
+ ?>
