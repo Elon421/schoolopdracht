c738d55dcccaf22b224c7c981cc5796398f0e927
Elon421 <132984242+Elon421@users.noreply.github.com>
2023-06-16 13:38:31 +0200

    Add files via upload

---
+++ functie.php
@@ -0,0 +1,41 @@
+<?php 
+//opdracht1
+// $tijd = date("G:i a");
+
+// if ($tijd < "12:00") {
+    // echo "Goedemorgen";
+// } elseif ($tijd < "18:00") {
+    // echo "Goedemiddag";
+// } else {
+    // echo "Goedenavond";
+// }
+//opdracht 2
+
+//function getallen($nummer1, $nummer2){
+   // $result = $nummer1 + $nummer2;
+   // return $result;
+//}
+//getallen(5,5);
+//echo "De resultaat is" . getallen(5,5);
+
+//opdracht3
+
+// $datumvannu = date("Y-m-d");
+// $einddatum = date('Y-m-d', strtotime('12/31'));
+// $verschil = date_diff(date_create($datumvannu), date_create($einddatum));
+
+// echo $verschil->format('%a');
+
+//opdracht4
+//function bereken_lengte($string) {
+   // $totale_lengte = 0;
+  //  foreach ($string as $string) {
+  //      $totale_lengte += strlen($string);
+ //   }
+ //   return $totale_lengte;
+//}
+//$array = array("Hallo", "wereld");
+//$lengte = bereken_lengte($array);
+//echo "Totale lengte: " . $lengte;
+
+?>
